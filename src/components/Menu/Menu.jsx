0fbb485242f4111f8ac2b70c617a57affa3c7958
cassius.avila <cassiusavila@gmail.com>
2021-04-10 20:46:03 -0300

    :sparkles: created menu component

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import styles from './Menu.module.scss'
+import { NavBar } from 'components'
+import IconButton from 'components/IconButton/IconButton'
+import { Close, Menu as MenuIcon } from 'assets/icons'
+
+function Menu() {
+  const [isOpen, setIsOpen] = useState(true)
+
+  const handleOpen = () => setIsOpen(!isOpen)
+
+  return isOpen ? (
+    <menu className={styles.menu} aria-hidden={isOpen}>
+      <IconButton onClick={() => handleOpen()} icon={<Close size="40" />} />
+      <NavBar />
+    </menu>
+  ) : (
+    <IconButton onClick={() => handleOpen()} icon={<MenuIcon size="40" />} />
+  )
+}
+
+export default Menu
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 import Button from './Button'
+import Input from './Input'
+import Menu from './Menu'
+import NavBar from './NavBar'
+import Switch from './Switch'
 
-export { Button }
+export { Button, Input, Menu, NavBar, Switch }
