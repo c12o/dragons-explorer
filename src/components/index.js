39745c1c4cffc0673f1bf41ee465bd3d585df947
cassius.avila <cassiusavila@gmail.com>
2021-04-11 15:40:21 -0300

    :recycle: :lipstic: improve styles declarations

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,11 @@
-import Button from './Button'
-import Input from './Input'
-import Menu from './Menu'
-import NavBar from './NavBar'
-import Switch from './Switch'
-import Header from './Header'
-import IconButton from './IconButton'
-import ListItem from './ListItem'
-import Logo from './Logo'
-import ToggleExhibitionListType from './ToggleExhibitionListType'
-
-export {
-  Button,
-  Input,
-  Menu,
-  NavBar,
-  Switch,
-  Logo,
-  Header,
-  ListItem,
-  IconButton,
-  ToggleExhibitionListType
-}
+export { default as Button } from './Button'
+export { default as Header } from './Header'
+export { default as IconButton } from './IconButton'
+export { default as Input } from './Input'
+export { default as ListItem } from './ListItem'
+export { default as Loader } from './Loader'
+export { default as Logo } from './Logo'
+export { default as Menu } from './Menu'
+export { default as NavBar } from './NavBar'
+export { default as Switch } from './Switch'
+export { default as ToggleExhibitionListType } from './ToggleExhibitionListType'
